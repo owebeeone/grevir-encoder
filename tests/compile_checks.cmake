599da9682e501c2decfa8d7436020b711c599460
Gianni Mariani <gianni@everbility.com>
2026-09-23 22:55:13 +1000

    Run compiler rejection probes with MSVC
    GWZ-Commit-ID: 01a0ce55-9f43-7775-8142-b4d4597d1253
    GWZ-Workspace-ID: ws_default
    GWZ-Origin-URL-Hash: sha256:b447322346850c84947a6704a11ee61707db25812f757b3276513c4757dadaad

--- tests/compile_checks.cmake
+++ tests/compile_checks.cmake
@@ -13,12 +13,13 @@ add_library(grevir_encoder_compile OBJECT native_compile.cpp ${header_sources})
 target_link_libraries(grevir_encoder_compile PRIVATE grevir::encoder)
 set_target_properties(grevir_encoder_compile PROPERTIES CXX_EXTENSIONS OFF)
 
-if(NOT CMAKE_CXX_COMPILER_ID MATCHES "^(AppleClang|Clang|GNU)$")
-  message(FATAL_ERROR "Encoder pin-claim probes currently require a Clang/GNU driver")
+if(NOT CMAKE_CXX_COMPILER_ID MATCHES "^(AppleClang|Clang|GNU|MSVC)$")
+  message(FATAL_ERROR "Encoder pin-claim probes require a supported C++ compiler driver")
 endif()
 add_custom_target(grevir_encoder_claim_checks ALL
   COMMAND "${CMAKE_COMMAND}"
     "-DCXX=${CMAKE_CXX_COMPILER}"
+      "-DCOMPILER_ID=${CMAKE_CXX_COMPILER_ID}"
     "-DINCLUDE_DIRS=$<TARGET_PROPERTY:grevir_encoder_compile,INCLUDE_DIRECTORIES>"
     "-DCASE_SOURCE=${CMAKE_CURRENT_SOURCE_DIR}/pin_claim_probe.cpp"
     "-DLOG_DIR=${CMAKE_CURRENT_BINARY_DIR}/claim-results"
